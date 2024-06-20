ace90b587a08cbc20248953520bacba1031ec50f
Luca Duarte <luca.duarte02@gmail.com>
2024-06-20 19:42:20 -0400

    developing home page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@600&family=Roboto+Condensed&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500;600&family=Roboto+Condensed&display=swap" rel="stylesheet"/>
       </head>
       <Navbar/>
       <div className="container">
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,3 +1,11 @@
+import './Home.css'
+
 export default function Home() {
-    
+   return (
+    <>
+      <h1 class="kanit-medium">Welcome to TSI at Western!</h1>
+      <h2>insert logo here</h2>
+      <h3>Club introduction here</h3>
+    </>
+   )
 }
