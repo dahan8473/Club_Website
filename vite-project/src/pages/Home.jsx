d50c8e2cd0e4842cc8362ff7b7203cba2f69c188
Luca Duarte <luca.duarte02@gmail.com>
2024-06-25 15:24:23 -0400

    home page graphics setup

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import './Home.css'
 import city from '../assets/images/city.png'
 import street from '../assets/images/Street.png'
+import other_street from '../assets/images/street2.png'
+import hospital from '../assets/images/Hospital.png'
 
 export default function Home() {
    return (
@@ -9,8 +11,10 @@ export default function Home() {
       <h1 class="kanit-medium">Welcome to TSI at Western!</h1>
       <h2 class="kanit-light">Providing technological solutions to nonprofit organizations</h2>
       <div class="graphics">
+        <img class="central-graphic" style={{width: "328px", height: "400px"}} src={other_street} />
+        <img class="central-graphic" style={{width: "400px", height: "400px"}} src={hospital} />
         <img class="central-graphic" style={{width: "400px", height: "400px"}} src={city} />
-        {/*<img class="central-graphic" style={{width: "400px", height: "400px"}} src={street} /> */}
+        <img class="central-graphic" style={{width: "400px", height: "400px"}} src={street} />
       </div>
     </>
    )
