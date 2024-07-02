5398195ac80f271012b8749180ff02375b3ff010
Justin <157526056+JXOHG@users.noreply.github.com>
2024-07-02 17:31:05 -0400

    Add files via upload

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,3 +1,25 @@
-export default function About() {
-    
+import './About.css'
+import logo from "../assets/images/tsi_logo_globe.png"
+
+
+export default function About(){
+    return(
+      <div>
+        <h1>About us..</h1>
+        <h2 class="text">At TSI, we are committed to providing tech solutions for organizations committed to social good. Our mission is to create opportunities for Western students to collaborate in a professional work environment while addressing the needs of organizations that are actively working to improve our society.</h2>
+      <img class="logo fade" src={logo}/>
+        <h1>Our Mission</h1>
+
+
+
+
+        
+      </div>
+      
+      
+     
+      
+      
+      
+    )
 }
