3138bae6c940aee4c09ff322fd54a4a4547273bd
Justin <157526056+JXOHG@users.noreply.github.com>
2024-07-20 15:27:56 -0400

    Update Home.jsx

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -9,9 +9,12 @@ export default function Home() {
    return (
     <>
       {/* Insert logo here*/}
-      <img class = "center" style = {{width: "300px", height: "300px"}}src={logo}/>
-      <h1 class="kanit-medium">Welcome to TSI at Western!</h1>
-      <h2 class="kanit-light">Providing technological solutions to nonprofit organizations</h2>
+      
+      <div class="frombottom">
+        <img class = "center" style = {{width: "300px", height: "300px"}}src={logo}/>
+        <h1 class="kanit-medium">Welcome to TSI at Western!</h1>
+      <h2 class="kanit-light">Providing technological solutions to nonprofit organizations</h2></div>
+      
       {/*<div class="graphics animated-element" >
         <img class="central-graphic" style={{width: "328px", height: "400px"}} src={other_street} />
         <img class="central-graphic" style={{width: "400px", height: "400px"}} src={hospital} />
