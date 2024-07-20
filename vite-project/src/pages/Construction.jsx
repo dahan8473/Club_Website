6dc37937b8f566ea56d3ffd8b2b37d45457e5434
Justin <157526056+JXOHG@users.noreply.github.com>
2024-07-20 16:45:47 -0400

    Create Construction.jsx

---
+++ vite-project/src/pages/Construction.jsx
@@ -0,0 +1,18 @@
+import construction from '../assets/images/construction.png'
+import logo from '../assets/images/logo.png'
+import './Construction.css'
+
+export default function Projects() {
+    return (
+        <>
+            <div className="projects-container">
+                <img className="graphics" style={{width: "520px", height: "500px"}} src={construction}/>
+                <div className="text-body">
+                    <h1 className="kanit-medium">This page is under construction...</h1>
+                    <h2 className="kanit-light">Keep posted to see what we will work on next!</h2>
+                    <img id="logo" style={{width: "400px", height: "300px"}}   src={logo}/>
+                </div>
+            </div>
+        </>
+    )
+}
