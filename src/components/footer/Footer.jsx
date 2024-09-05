209c03b1cebf623b6901423c42082c6e76d8cd08
Ethan Liu <108840760+EthanL818@users.noreply.github.com>
2024-09-05 13:44:19 -0400

    bug fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='footer__container container'>
-        <h1 className='footer__title'>Tech for Social Impact</h1>
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Tech for Social Impact</h1>
 
-        <div className='footer__social'>
-          <a href='https://www.instagram.com/westerntsi/' className='home__social-icon'>
-            <i className='uil uil-instagram'></i>
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/westerntsi/"
+            className="home__social-icon"
+            target="_blank"
+          >
+            <i className="uil uil-instagram"></i>
           </a>
 
-          <a href='https://www.linkedin.com/company/techforsocialimpact/mycompany/' className='home__social-icon'>
-            <i className='uil uil-linkedin'></i>
+          <a
+            href="https://www.linkedin.com/company/techforsocialimpact/mycompany/"
+            className="home__social-icon"
+            target="_blank"
+          >
+            <i className="uil uil-linkedin"></i>
           </a>
 
-          <a href='https://discord.gg/qxk7BCzH' className='home__social-icon'>
-            <i className='uil uil-discord'></i>
+          <a
+            href="https://discord.gg/A8fcwWx4"
+            className="home__social-icon"
+            target="_blank"
+          >
+            <i className="uil uil-discord"></i>
           </a>
         </div>
 
-        <span className='footer__copy'>
-          &#169; UWO TSI 2024
-        </span>
+        <span className="footer__copy">&#169; UWO TSI 2024</span>
       </div>
     </footer>
   );
