45c969e1024eff5fb2c4d9d443986c486e5e0998
Justin <157526056+JXOHG@users.noreply.github.com>
2025-03-16 17:38:19 -0400

    A LOT OF DEBUGGING

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,24 +10,27 @@ const Footer = () => {
         <div className="footer__social">
           <a
             href="https://www.instagram.com/westerntsi/"
-            className="home__social-icon"
+            className="footer__social-link"
             target="_blank"
+            rel="noreferrer"
           >
             <i className="uil uil-instagram"></i>
           </a>
 
           <a
             href="https://www.linkedin.com/company/techforsocialimpact/mycompany/"
-            className="home__social-icon"
+            className="footer__social-link"
             target="_blank"
+            rel="noreferrer"
           >
             <i className="uil uil-linkedin"></i>
           </a>
 
           <a
             href="https://discord.gg/A8fcwWx4"
-            className="home__social-icon"
+            className="footer__social-link"
             target="_blank"
+            rel="noreferrer"
           >
             <i className="uil uil-discord"></i>
           </a>
